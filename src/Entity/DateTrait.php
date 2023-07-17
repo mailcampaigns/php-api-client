c4f4abba8128d86bd957ca36eacf945a250c6c96
Bert van der Genugten <bert@mailcampaigns.nl>
2023-07-17 15:48:27 +0200

    -

--- src/Entity/DateTrait.php
+++ src/Entity/DateTrait.php
@@ -8,8 +8,8 @@ use DateTimeInterface;
 
 trait DateTrait
 {
-    protected ?DateTimeInterface $createdAt;
-    protected ?DateTimeInterface $updatedAt;
+    protected ?DateTimeInterface $createdAt = null;
+    protected ?DateTimeInterface $updatedAt = null;
 
     public function getCreatedAt(): ?DateTimeInterface
     {
